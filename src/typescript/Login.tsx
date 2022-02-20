70922dfa67add7c9a3d236c147f0c31857ef7c70
Giuseppe Caraffi <giuseppe@iMac-di-Mauro.lan>
2022-02-20 17:40:41 +0100

    aggiunta la comunicazione tra client e server

--- src/typescript/Login.tsx
+++ src/typescript/Login.tsx
@@ -7,23 +7,52 @@ const Login = () => {
     const emailInput = useRef();
     const passwordInput = useRef();
 
-    function handleLoginForm()
+    const handleLoginForm = async (e) =>
     {
+        e.preventDefault(); //evita di ricaricare la pagina
+        const url = "http://localhost:80/login";
         const email = emailInput.current.value;
-        var password = passwordInput.current.value;
+        const password = passwordInput.current.value;
+        const corpo = {email: email, password:password};
+         //FETCH
+        e.preventDefault(); //evita di ricaricare la pagina
+        
 
-        password = sha3_512(password); //trasforma la password in un hash di 128 caratteri
-        document.getElementById('password')['value'] = password;
+        try
+        {
+            const richiesta = {
+                
+                headers: {
+                    "Content-Type": "text/plain",
+                    "accept": "application/json"
+                },
+                
+                body: JSON.stringify(corpo),
+                //mode: ,
+                method: "POST"
+            }
+
+            fetch (url, richiesta).then(data => {return data.text()})
+            .then(res => {console.log(res)})
+            .then(error => {alert("errore: "+error)});
+        }catch (err)
+        {
+            alert(err);
+        }
+        
+
+        
     }
 
     return (
         <div className="divLogin">
             <h1>Login</h1>
 
-            <form action='http://localhost:80/login' method='POST'>
+            <form onSubmit = {handleLoginForm}>
                 <input className='testo' type='text' ref={emailInput} id='email' name='email' placeholder='Email' required /> <br /> <br />
                 <input className='testo' type='password' ref={passwordInput} id='password' name='password' placeholder='Password' required /> <br /> <br />
-                <input id='bottone' type='submit' onClick={handleLoginForm}/>
+                {/*<input id='bottone' type='submit' onClick={handleLoginForm}/>*/}
+                <button id='bottone' type='submit'> Invia</button>
             </form>
         </div> 
         
