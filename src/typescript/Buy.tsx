6b369c90ea54d40065a47682f98e1d9204186a6f
flemmi <44188804+flemmi@users.noreply.github.com>
2022-02-28 22:33:54 +0100

    aggiunto il cambio valuta (senza i controlli sul massimo)

--- src/typescript/Buy.tsx
+++ src/typescript/Buy.tsx
@@ -1,13 +1,44 @@
 import '../css/Form.css';
+import axios from 'axios';
 const Buy = () => {
 
     const max = 32;
+
+    const handleLoginForm = async (e) =>
+    {
+        e.preventDefault(); //evita di ricaricare la pagina
+        const url = "http://localhost:80/buy";
+
+        const quantita_spesa = e.target.valore.value;
+        const valuta = e.target.valuta.value;
+
+        const corpo = {
+            quantita_spesa: quantita_spesa,
+            valuta: valuta
+        };
+
+        const risposta = await axios.post(url, corpo);
+        const oggetto = JSON.parse(risposta.data);
+
+        if (oggetto["ridirezione"])
+        {
+            window.location.href = 'http://localhost:3000/';
+        }
+        else
+        {
+            alert (oggetto["messaggio"]);
+            if (oggetto["isTuttoOk"])
+            {
+                window.location.href = 'http://localhost:3000/home';
+            }
+        }
+    }
 
     return (
         <div className="divForm">
             <h1>Converti Valuta</h1>
 
-            <form id = "form">
+            <form id = "form" onSubmit = {handleLoginForm}>
                 <input className='testo' type='number' id='valore' name='valore' placeholder="Valore da convertire nell'altra valuta" min='1' max={max} step="0.01" required/><br /><br />
                 
                 <select id="valuta" name="valuta" form='form'>
--- src/typescript/Login.tsx
+++ src/typescript/Login.tsx
@@ -18,49 +18,7 @@ const Login = () => {
         password = sha3_512(password); //cifro la password
         const corpo = {email: email, password: password, withCredentials: true}; //creo l'oggetto json da inviare al server
         
-        /*
         //INVIO I DATI
-        e.preventDefault(); //evita di ricaricare la pagina  
-        try
-        {
-            const richiesta = { //creo la richiesta http
-                headers: {
-                    "Content-Type": "application/json",
-                    "accept": "text/plain",
-                    "Access-Control-Allow-Origin": "*", //serve per la comunicazione client server che stanno in posti diversi
-                    "Access-Control-Allow-Methods": "POST",  
-                },
-                body: JSON.stringify(corpo),
-                method: "POST"
-            }
-
-            
-            const risposta = await fetch(url, richiesta);
-            if (risposta.ok) // se è andato tutto ok
-            {
-                const stringa = await risposta.json(); //ricevo la risposta
-                const ricevuto = JSON.parse(stringa); //traduco la stringa in oggetto json
-                if (!ricevuto["isTuttoOk"]) //se c'è un errore lo comunico, altrimenti procedo
-                {
-                    alert(ricevuto["token"]);
-                }
-                else //se avviene con successo il login
-                {
-                    alert("autenticazione avvenuta");
-                }
-            }
-            else // se c'è stato qualche errore nella comunicazione
-            {
-                alert("errore nella comunicazione con il server");
-            }
-        }catch (err)
-        {
-            alert(err);
-        }
-        */
-
-        
-        //AXIOS
         const risposta = await axios.post(url, corpo);
         const oggetto = JSON.parse(risposta.data);
         if (!oggetto["isTuttoOk"]) //se c'è un errore lo comunico, altrimenti procedo
@@ -74,10 +32,6 @@ const Login = () => {
             window.location.replace(link);
             //axios.post("http://localhost:80/ciaoo", {email: email, password: password, withCredentials: true});
         }
-        
-        //controllo di aver ricevuto il cookie (inutile)
-        //const risposta2 = await axios.get("http://localhost:80/ciaoo");
-        //alert(risposta2.data);
     }
 
     return (
@@ -87,7 +41,6 @@ const Login = () => {
             <form onSubmit = {handleLoginForm}>
                 <input className='testo' type='text' ref={emailInput} id='email' name='email' placeholder='Email' required /> <br /> <br />
                 <input className='testo' type='password' ref={passwordInput} id='password' name='password' placeholder='Password' required /> <br /> <br />
-                {/*<input id='bottone' type='submit' onClick={handleLoginForm}/>*/}
                 <button id='bottone' type='submit'> Invia</button>
             </form>
         </div> 
